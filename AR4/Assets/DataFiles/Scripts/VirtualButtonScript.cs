12a180503f9016ec23225926aff773e6fadbeaad
Rohan Maharjan <Rohan9841@gmail.com>
2018-05-29 00:15:16 -0500

    Add first virtual button without db.lock

---
+++ AR4/Assets/DataFiles/Scripts/VirtualButtonScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VirtualButtonScript : MonoBehaviour,IVirtualButtonEventHandler { //IVirtualButtonEventHandler is an interface
+
+    public GameObject cubeObject, sphereObject;
+
+    //VirtualButtonBehavior is a script and we need a datatype of that script to store the script
+    VirtualButtonBehaviour vbb;
+
+	// Use this for initialization
+	void Start () {
+        //storing the VirtualButtonBehavior script in vbb
+        vbb = GetComponentInChildren<VirtualButtonBehaviour>();
+
+        //Registering the virtualButton
+        vbb.RegisterEventHandler(this);
+
+        cubeObject.SetActive(true);
+        sphereObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //OnButtonPressed is an abstract method of IVirtualButtonEventHandler
+    public void OnButtonPressed(VirtualButtonBehaviour vb) //VirtualButtonBehavior is a datatype that gives the name of the virual butotn pressed   
+    {
+        cubeObject.SetActive(false);
+        sphereObject.SetActive(true);
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        cubeObject.SetActive(true);
+        sphereObject.SetActive(false);
+    }
+}
